0d9be4314e814f19a2b7e94bff02da6934da14a9
PronskayaAnastasiya <100492707+PronskayaAnastasiya@users.noreply.github.com>
2022-06-16 18:28:08 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+let addMessage=document.querySelector('.message');
+let addButton=document.querySelector('.add');
+let todo=document.querySelector('.todo');
+let todoList=[];
+addButton.addEventListener('click',function(){
+    let newTodo={
+        todo:addMessage.value,
+        checked: false,
+        important: false
+    };
+    todoList.push(newTodo);
+    console.log(todoList);
+});
+
+function displayMessages(){
+    todoList.forEach(function(item,i){
+        console.log(i)
+    });
+}
